c4cbc38cb979b0c07e818e12debbc5a084d9527e
Daggron <returnofking04@gmail.com>
2019-12-03 19:10:41 +0530

    Handled cors

--- api/index.js
+++ api/index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const cors = require('cors');
+
+
 const redis = require('redis');
 client = redis.createClient();
+
 
 
 const {promisify} = require('util');
 const getAsync = promisify(client.get).bind(client);
 
 
+app.use(cors());
 
 
 const port = process.env.PORT || 5000;
